7697f6e026ba9c014182eed28b6c5d624b13a4b3
jet_demin <ay.dyomin@jet.msks.u>
2017-08-27 04:50:49 +0300

    Реализация проверки соединения

--- app/src/main/java/com/dyomin/android/hfsaclient/MainActivity.java
+++ app/src/main/java/com/dyomin/android/hfsaclient/MainActivity.java
@@ -2,12 +2,59 @@ package com.dyomin.android.hfsaclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.response_text);
+        editText = (EditText) findViewById(R.id.editText);
+        Button buttonSend = (Button) findViewById(R.id.button);
+
+        queue = Volley.newRequestQueue(this);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = editText.getText().toString();
+                processRequest(url);
+            }
+        });
+    }
+
+    private void processRequest(String url) {
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
